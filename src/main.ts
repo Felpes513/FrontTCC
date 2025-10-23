2eb13c4af87c538ca5391255f064f0bf7922be9d
Felipe Souza Moreira <felipemoreira2003@gmail.com>
2025-10-23 14:24:42 -0300

    wait: A espera do mentor

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { register } from 'swiper/element/bundle';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch(err => console.error(err));
+register(); // registro global dos web components
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
