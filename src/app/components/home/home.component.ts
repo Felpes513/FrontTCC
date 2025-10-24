fdbcd754606e21d912380a027edf6926b4548c14
Sergio Bastos <sergio.augusto@mobilemed.com.br>
2025-10-24 10:09:47 -0300

    carroussel resolvido

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -17,28 +17,28 @@ export class HomeComponent {
       content:
         'Desenvolva projetos inovadores que contribuem para o avanço da ciência e tecnologia.',
       icon: 'fas fa-flask',
-      img: "/public/assets/Barcelona.png",
+      img: "../../../assets/Barcelona.png",
     },
     {
       title: 'Orientação Especializada',
       content:
         'Conte com professores experientes para guiar sua jornada acadêmica e científica.',
       icon: 'fas fa-user-friends',
-      img: '/assets/centro.jpg',
+      img: '../../../assets/centro.jpg',
     },
     {
       title: 'Bolsas de Estudo',
       content:
         'Oportunidades de bolsas para dedicação exclusiva aos seus projetos de pesquisa.',
       icon: 'fas fa-graduation-cap',
-      img: '/assets/conceicao.png',
+      img: '../../../assets/conceicao.png',
     },
     {
       title: 'Publicações Científicas',
       content:
         'Publique seus resultados em revistas e eventos científicos de renome nacional e internacional.',
       icon: 'fas fa-book-open',
-      img: '/assets/Barcelona.png',
+      img: '../../../assets/Barcelona.png',
     },
   ];
 }
